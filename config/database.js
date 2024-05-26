09e4d885c68cfd6c207b63ab965f0bb6b22d1e5f
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2024-05-26 05:31:10 +0100

    fix: restructure code.

--- config/database.js
+++ config/database.js
@@ -6,7 +6,6 @@ const connectDatabase = () => {
     .connect(
       `mongodb+srv://${process.env.MONGO_USER}:${process.env.DATABASE_PASSWORD}@recorder.sezbjvk.mongodb.net/?retryWrites=true&w=majority&appName=recorder`
     )
-    // .connect("mongodb://127.0.0.1:27017/login")
     .then(() => {
       console.log("[200] SUCCESS: Database Connected");
     })
